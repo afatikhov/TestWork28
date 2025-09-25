8f1b8023b66925806b481978db2736abfb87ddb7
Artur <arturfatikhov11@gmail.com>
2025-09-25 14:28:42 +0300

    added page loading exception

--- src/exceptions/custom_exceptions.py
+++ src/exceptions/custom_exceptions.py
@@ -1,3 +1,8 @@
 class NoDataInsertedException(BaseException):
     def __init__(self, message: str = "No data was inserted!"):
         super().__init__(message)
+
+class PageLoadException(BaseException):
+    def __init__(self, url: str):
+        message: str = f"Error in loading page: {url}"
+        super().__init__(message)
